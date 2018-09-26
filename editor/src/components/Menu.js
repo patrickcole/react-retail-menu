84a790f1d72d3c5ce39c4ac907697d2151203e3b
Patrick Cole <hpcmobiledev@gmail.com>
2018-09-26 14:11:21 -0400

    Provides the ability to dynamically add products w/ state, input controls now saving state

--- editor/src/components/Menu.js
+++ editor/src/components/Menu.js
@@ -4,10 +4,37 @@ import Product from './Product';
 
 export default class Menu extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [
+        {
+          title: "Chocolate Cake",
+          description: "Mmmm delicious!",
+          price: "2.49"
+        },
+        {
+          title: "Vanilla Cake",
+          description: "Mmmm delicious!",
+          price: "2.49"
+        }
+      ]
+    }
+  }
   render() {
     return (
       <ul>
-        <Product title="Chocolate Cake" description="Mmmmm delicious!" price="2.49" />
+        { this.state.products.map( (product, index) => {
+
+          return (
+            <Product 
+              key={`product${index}`}
+              title={product.title} 
+              description={product.description} 
+              price={product.price} 
+            />
+          )
+        })}
       </ul>
     )
   }
